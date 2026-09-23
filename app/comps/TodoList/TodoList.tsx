417742b2877eb58e61390af3653e9b08382f0755
DinosMpo <dinos7591@yahoo.gr>
2026-09-23 18:00:55 +0300

    create the add todo functionality

--- app/comps/TodoList/TodoList.tsx
+++ app/comps/TodoList/TodoList.tsx
@@ -1,27 +1,29 @@
-import React from "react";
+"use client";
+
 import styles from "./TodoList.module.css";
 import Todo from "../Todo/Todo";
 
-export default function TodoList() {
-  const todoItems = [
-    "Complete online Javascript course",
-    "Jog around the park 3x",
-    "10 minutes meditation",
-    "Read for 1 hour",
-    "Pick up grocereies",
-    "Complete Todo App on Fronend Mentor",
-  ];
-
+export default function TodoList({ todoItems, setTodoItems }) {
+  let itemsLeft = 0;
   const todoItemsList = todoItems.map((item, key) => {
-    return <Todo key={key} item={item} />;
+    if (!item.checked) itemsLeft++;
+    return (
+      <Todo
+        key={key}
+        index={key}
+        item={item}
+        setTodoItems={setTodoItems}
+        todoItems={todoItems}
+      />
+    );
   });
 
   return (
     <div id={styles["todo-list-container"]}>
-      <div>{todoItemsList}</div>
+      <div id={styles["todo-list-wrapper"]}>{todoItemsList.reverse()}</div>
 
       <div id={styles["todo-options"]}>
-        <div>{todoItems.length} items left</div>
+        <div>{itemsLeft} items left</div>
         <div id={styles["todo-options-wrapper"]}>
           <div>All</div>
           <div>Active</div>
