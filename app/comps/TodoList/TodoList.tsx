6b7c071e227d2104679fed48bb6c5236a7f6e772
DinosMpo <dinos7591@yahoo.gr>
2026-09-24 15:48:29 +0300

    create the functionality of filters and the clear completed

--- app/comps/TodoList/TodoList.tsx
+++ app/comps/TodoList/TodoList.tsx
@@ -1,35 +1,115 @@
 "use client";
 
+import { useState } from "react";
 import styles from "./TodoList.module.css";
 import Todo from "../Todo/Todo";
 
 export default function TodoList({ todoItems, setTodoItems }) {
+  const [activeCategorie, setActiveCategorie] = useState("all");
+
   let itemsLeft = 0;
-  const todoItemsList = todoItems.map((item, key) => {
-    if (!item.checked) itemsLeft++;
-    return (
-      <Todo
-        key={key}
-        index={key}
-        item={item}
-        setTodoItems={setTodoItems}
-        todoItems={todoItems}
-      />
-    );
-  });
+  let todoItemsList = [];
+
+  if (activeCategorie == "all") {
+    todoItemsList = todoItems.map((item, key) => {
+      if (!item.checked) itemsLeft++;
+      return (
+        <Todo
+          key={key}
+          index={key}
+          item={item}
+          setTodoItems={setTodoItems}
+          todoItems={todoItems}
+        />
+      );
+    });
+  } else if (activeCategorie == "active") {
+    todoItemsList = todoItems.map((item, key) => {
+      if (!item.checked) {
+        itemsLeft++;
+        return (
+          <Todo
+            key={key}
+            index={key}
+            item={item}
+            setTodoItems={setTodoItems}
+            todoItems={todoItems}
+          />
+        );
+      }
+    });
+  } else if (activeCategorie == "completed") {
+    todoItemsList = todoItems.map((item, key) => {
+      if (item.checked) {
+        itemsLeft++;
+        return (
+          <Todo
+            key={key}
+            index={key}
+            item={item}
+            setTodoItems={setTodoItems}
+            todoItems={todoItems}
+          />
+        );
+      }
+    });
+  }
+
+  const clearCompleted = () => {
+    let oldTodoItems = [...todoItems];
+    console.log("oldTodoItems");
+    console.log(oldTodoItems);
+    let newTodoItems: any[] = [];
+    oldTodoItems.map((todo, key) => {
+      // console.log("todo");
+      if (!todo.checked) {
+        console.log(key);
+        console.log(!todo.checked);
+        newTodoItems.push(todo);
+      }
+    });
+
+    // console.log(newTodoItems);
+    setTodoItems(newTodoItems);
+  };
+
+  console.log(todoItems);
 
   return (
     <div id={styles["todo-list-container"]}>
       <div id={styles["todo-list-wrapper"]}>{todoItemsList.reverse()}</div>
+      <div id={styles["todo-options-container"]}>
+        <div id={styles["todo-options"]}>
+          <div>{itemsLeft} items left</div>
+          <div id={styles["todo-options-wrapper"]}>
+            <div
+              id={activeCategorie == "all" ? `${styles["active-all"]}` : ""}
+              onClick={() => setActiveCategorie("all")}
+            >
+              All
+            </div>
+            <div
+              id={
+                activeCategorie == "active" ? `${styles["active-active"]}` : ""
+              }
+              onClick={() => setActiveCategorie("active")}
+            >
+              Active
+            </div>
+            <div
+              id={
+                activeCategorie == "completed"
+                  ? `${styles["active-completed"]}`
+                  : ""
+              }
+              onClick={() => setActiveCategorie("completed")}
+            >
+              Completed
+            </div>
+          </div>
 
-      <div id={styles["todo-options"]}>
-        <div>{itemsLeft} items left</div>
-        <div id={styles["todo-options-wrapper"]}>
-          <div>All</div>
-          <div>Active</div>
-          <div>Completed</div>
+          <div onClick={() => clearCompleted()}>Clear Comleted</div>
         </div>
-        <div>Clear Comleted</div>
       </div>
     </div>
   );
